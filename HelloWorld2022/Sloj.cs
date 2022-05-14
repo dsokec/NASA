cfc422ae0b0a0a300c6b7eaf23790207fc563366
Dino Šokec <dino.sokec97@gmail.com>
2022-05-14 22:41:37 +0200

    sloj
    ukljuci klasu u preostale klasu da bi bila korisna

---
+++ HelloWorld2022/Sloj.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GradjevniDijelovi
+{
+    public class Sloj
+    {
+        private float _debljina;
+
+        public int Id { get; set; }
+        public Dio GradjevniDio { get; set; }
+        public Materijal MaterijalSloja { get; set; }
+        public float Debljina
+        {
+            get
+            {
+                return _debljina;
+            }
+            set
+            {
+                _debljina = Math.Max(0, value);
+            }
+        }
+
+        public Sloj(int id, float debljina, Dio gradjevniDio, Materijal materijal)
+        {
+            Id = id;
+            Debljina = debljina;
+            GradjevniDio = gradjevniDio;
+            MaterijalSloja = materijal;
+            GradjevniDio.Slojevi.Add(this);
+        }
+    }
+}
