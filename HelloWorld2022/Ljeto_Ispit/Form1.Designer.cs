8391ddbb3c9dc904eed872de6a5d5021d6a31b21
Dino Šokec <dino.sokec97@gmail.com>
2022-05-14 23:26:36 +0200

    Novi projekt
    Ispit ljeto

---
+++ HelloWorld2022/Ljeto_Ispit/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Ljeto_Ispit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCountRows = new System.Windows.Forms.Label();
+            this.txtUnos = new System.Windows.Forms.TextBox();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.btnShowAll = new System.Windows.Forms.Button();
+            this.dgvProducts = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cmbFilterBy = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "FilterBy";
+            // 
+            // lblCountRows
+            // 
+            this.lblCountRows.AutoSize = true;
+            this.lblCountRows.Location = new System.Drawing.Point(794, 118);
+            this.lblCountRows.Name = "lblCountRows";
+            this.lblCountRows.Size = new System.Drawing.Size(127, 20);
+            this.lblCountRows.TabIndex = 0;
+            this.lblCountRows.Text = "Showing records";
+            this.lblCountRows.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtUnos
+            // 
+            this.txtUnos.Location = new System.Drawing.Point(235, 25);
+            this.txtUnos.Name = "txtUnos";
+            this.txtUnos.Size = new System.Drawing.Size(143, 26);
+            this.txtUnos.TabIndex = 1;
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(384, 21);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(85, 35);
+            this.btnFilter.TabIndex = 2;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            this.btnFilter.Click += new System.EventHandler(this.btnFilter_Click);
+            // 
+            // btnShowAll
+            // 
+            this.btnShowAll.Location = new System.Drawing.Point(384, 62);
+            this.btnShowAll.Name = "btnShowAll";
+            this.btnShowAll.Size = new System.Drawing.Size(85, 31);
+            this.btnShowAll.TabIndex = 2;
+            this.btnShowAll.Text = "Show All";
+            this.btnShowAll.UseVisualStyleBackColor = true;
+            this.btnShowAll.Click += new System.EventHandler(this.btnShowAll_Click);
+            // 
+            // dgvProducts
+            // 
+            this.dgvProducts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProducts.Location = new System.Drawing.Point(21, 157);
+            this.dgvProducts.Name = "dgvProducts";
+            this.dgvProducts.RowHeadersWidth = 62;
+            this.dgvProducts.RowTemplate.Height = 28;
+            this.dgvProducts.Size = new System.Drawing.Size(1046, 433);
+            this.dgvProducts.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbFilterBy);
+            this.groupBox1.Controls.Add(this.btnShowAll);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnFilter);
+            this.groupBox1.Controls.Add(this.txtUnos);
+            this.groupBox1.Location = new System.Drawing.Point(21, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(486, 111);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filter";
+            // 
+            // cmbFilterBy
+            // 
+            this.cmbFilterBy.FormattingEnabled = true;
+            this.cmbFilterBy.Location = new System.Drawing.Point(78, 23);
+            this.cmbFilterBy.Name = "cmbFilterBy";
+            this.cmbFilterBy.Size = new System.Drawing.Size(151, 28);
+            this.cmbFilterBy.TabIndex = 3;
+            this.cmbFilterBy.SelectedIndexChanged += new System.EventHandler(this.cmbFilterBy_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 602);
+            this.Controls.Add(this.dgvProducts);
+            this.Controls.Add(this.lblCountRows);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Products";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProducts)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblCountRows;
+        private System.Windows.Forms.TextBox txtUnos;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.Button btnShowAll;
+        private System.Windows.Forms.DataGridView dgvProducts;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbFilterBy;
+    }
+}
+
---
+++ HelloWorld2022/Ljeto_Ispit/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ljeto_Ispit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UcitavanjePodatakaBP();
+            UcitavanjePodatakaComboBox();
+        }
+
+        private void UcitavanjePodatakaComboBox()
+        {
+            cmbFilterBy.Items.Add("ProductID");
+            cmbFilterBy.Items.Add("Name");
+            cmbFilterBy.Items.Add("Product Number");
+
+            cmbFilterBy.SelectedIndex = 0;
+        }
+
+        private void UcitavanjePodatakaBP()
+        {
+            using (var db = new entities())
+            {
+                var query = from x in db.Product
+                            select x;
+                var countAllLoading = (from x in db.Product
+                             select x).Count();
+                dgvProducts.DataSource = query.Select(x => new { ProductID = x.ProductID, Name = x.Name, ProductNumber = x.ProductNumber, 
+                    StandardCost = x.StandardCost, ListPrice = x.ListPrice}).ToList();
+
+                lblCountRows.Text = String.Format($"Showing {countAllLoading} out of {countAllLoading} records");
+            }
+        }
+
+        public int UkupanBrojRedakaUTablici()
+        {
+            using (var db = new entities())
+            {
+                var countAllLoading = (from x in db.Product
+                                       select x).Count();
+                return countAllLoading;
+            }
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            using (var db = new entities())
+            {
+                var query = from x in db.Product
+                            select x;
+
+                dgvProducts.DataSource = query.Select(o => new {
+                    ProductID = o.ProductID,
+                    Name = o.Name,
+                    ProductNumber = o.ProductNumber,
+                    StandardCost = o.StandardCost,
+                    ListPrice = o.ListPrice
+                }).ToList();
+
+                lblCountRows.Text = String.Format($"Showing {UkupanBrojRedakaUTablici()} out of {UkupanBrojRedakaUTablici()} records");
+            }
+        }
+
+        private void cmbFilterBy_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FiltiranjeTabliceComboBoxTextBox();
+        }
+
+        private void FiltiranjeTabliceComboBoxTextBox()
+        {
+            int odabir = cmbFilterBy.SelectedIndex;
+            IzvrsavanjeOpcijeComboBoxa(odabir);
+        }
+
+        private void IzvrsavanjeOpcijeComboBoxa(int odabir)
+        {
+            switch (odabir)
+            {
+                case 0:
+                    FiltarPoProductID();
+                    break;
+                case 1:
+                    FiltarPoName();
+                    break;
+                case 2:
+                    FiltarPoProductNumber();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void FiltarPoProductNumber()
+        {
+            using (var db = new entities())
+            {
+                string uneseniZnak = txtUnos.Text.Trim();
+                var query = from x in db.Product
+                            where x.ProductNumber.Contains(uneseniZnak)
+                            select x;
+                var brojRedaka = (from x in db.Product
+                                  where x.ProductNumber.Contains(uneseniZnak)
+                                  select x).Count();
+
+                lblCountRows.Text = String.Format($"Showing {brojRedaka} out of {UkupanBrojRedakaUTablici()} records");
+
+                dgvProducts.DataSource = query.Select(y => new { ProductID = y.ProductID, Name = y.Name, ProductNumber = y.ProductNumber, 
+                StandardCost = y.StandardCost, ListPrice = y.ListPrice}).ToList();
+            }
+        }
+
+        private void FiltarPoName()
+        {
+            using (var db = new entities())
+            {
+                string uneseniZnak = txtUnos.Text.Trim();
+                var query = from x in db.Product
+                            where x.Name.Contains(uneseniZnak)
+                            select x;
+
+                var brojRedaka = (from x in db.Product
+                                  where x.Name.Contains(uneseniZnak)
+                                  select x).Count();
+
+                dgvProducts.DataSource = query.Select(y => new {
+                    ProductID = y.ProductID,
+                    Name = y.Name,
+                    ProductNumber = y.ProductNumber,
+                    StandardCost = y.StandardCost,
+                    ListPrice = y.ListPrice
+                }).ToList();
+
+                lblCountRows.Text = String.Format($"Showing {brojRedaka} out of {UkupanBrojRedakaUTablici()} records");
+            }
+        }
+
+        private void FiltarPoProductID()
+        {
+            using (var db = new entities())
+            {
+                string uneseniZnak = txtUnos.Text.Trim();
+                var query = from x in db.Product
+                            where (x.ProductID).ToString().Contains(uneseniZnak)
+                            select x;
+                var brojRedaka = (from x in db.Product
+                                  where (x.ProductID).ToString().Contains(uneseniZnak)
+                                  select x).Count();
+
+                dgvProducts.DataSource = query.Select(y => new {
+                    ProductID = y.ProductID,
+                    Name = y.Name,
+                    ProductNumber = y.ProductNumber,
+                    StandardCost = y.StandardCost,
+                    ListPrice = y.ListPrice
+                }).ToList();
+
+                lblCountRows.Text = String.Format($"Showing {brojRedaka} out of {UkupanBrojRedakaUTablici()} records");
+            }
+        }
+
+        private void btnFilter_Click(object sender, EventArgs e)
+        {
+            FiltiranjeTabliceComboBoxTextBox();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
