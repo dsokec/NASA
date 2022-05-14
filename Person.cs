704cab33d887c1ab4a3ca48c6fc6500e2df4c5c1
Dino Šokec <dino.sokec97@gmail.com>
2022-05-14 22:58:26 +0200

    još nove dvije klase hehe
    samo jako

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nasljedivanje
+{
+    // vidim da ova klasa nista od nikoga drugog ne nasljeduje
+    // ova klasa je polaznica za druge klase
+    public class Person
+    {
+        protected string RNO = "44";
+        protected string name = "ram";
+        /* 
+         * samo ovdje imam ključnu riječ virtual
+         * jer se smatra da je ovo bazna klasa
+         * koja daje nasljeđe preostalim klasama
+         * klase su stud i student
+         */
+        public virtual void GetInfo() {
+            //Console.WriteLine("Name: {0}", name);
+            // prethodni redak mogao sam napisati i ovako
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine("RNO: {0}", RNO);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nasljedivanje
+{
+    /* 
+     * prema glavnom programu ovdje ulazim unutra i nasljedujem podatke
+     * od klase Person
+     */
+    public class Student : Person
+    {
+        public string id = "ABC";
+        // imamo nadjačavanje metode jer ulazimo u metodu bazne klase
+        public override void GetInfo()
+        {
+            // ključna riječ base automatski se generira
+            base.GetInfo();
+
+            // ovo se normalno ispisuje u ovoj klasi
+            Console.WriteLine("Student ID: {0}", id);
+        }
+    }
+}
