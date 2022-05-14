53b97f61a63dd53ed08b408261b29f994515df6a
Dino Šokec <dino.sokec97@gmail.com>
2022-05-14 22:50:11 +0200

    Add files via upload

---
+++ Film.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glumci_i_filmovi.Models
+{
+    public class Film
+    {
+        public string Naslov;
+        public int Godina;
+
+        public List<Glumac> Glumci = new List<Glumac>();
+        public static List<Film> Filmovi = new List<Film>();
+        public Film(string naslov, int godina)
+        {
+            this.Naslov = naslov;
+            this.Godina = godina;
+            Filmovi.Add(this);
+        }
+        public void DodajGlumca(Glumac glumac)
+        {
+            if (!Glumci.Contains(glumac))
+            {
+                Glumci.Add(glumac);
+            }
+        }
+        public string Ispis()
+        {
+            string returnMe = "";
+            returnMe = this.Naslov + " " + this.Godina.ToString() +
+                "(";
+            foreach (Glumac item in this.Glumci)
+            {
+                returnMe += item.Ispis() + ", ";
+            }
+            return returnMe + ")";
+        }
+    }
+}
